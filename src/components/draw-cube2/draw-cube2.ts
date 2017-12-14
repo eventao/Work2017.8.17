2b933ff6371fdb906576874179dff3c25a34397f
evenyuan <Even_T_Yuan@manulife.com>
2017-12-14 18:15:01 +0800

    版本控制！

---
+++ src/components/draw-cube2/draw-cube2.ts
@@ -0,0 +1,50 @@
+import {Component, ElementRef} from '@angular/core';
+
+
+@Component({
+  selector: 'draw-cube2',
+  templateUrl: 'draw-cube2.html'
+})
+export class DrawCube2Component {
+
+  constructor(public elementRef:ElementRef) {}
+
+  initCanvasSize(){
+
+  }
+
+  initWebGl(){
+
+  }
+
+  initViewport(gl,canvas){}
+
+  initMatrices(canvas){
+
+  }
+
+  createCube(gl){
+
+  }
+
+  createShader(gl,str,type){
+
+  }
+
+  handleTextureLoaded(gl,texture){
+
+  }
+
+  initShader(gl){
+
+  }
+
+  initTexture(gl){
+
+  }
+
+  draw(gl,cube){
+
+  }
+
+}
