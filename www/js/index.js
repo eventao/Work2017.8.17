7de7837d01f3f17c0f26ad5ba311f7d0f7fcb6ba
evenyuan <330293577@qq.com>
2017-12-26 21:56:35 +0800

    添加动画！

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,14 @@
-let app = {
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-    onDeviceReady: function() {
-
-    },
-
-    pageLoaded:function(){
-        let init = new pinkCalf.placeInit(".app");
-        init.init();
-    }
-};
+class App{
+  constructor(){}
+  initialize(){
+    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+  }
+  onDeviceReady(){}
+  pageLoaded(){
+    let init = new pinkCalf.placeInit(".app");
+    init.init();
+  }
+}
+let app = new App();
 app.initialize();
 app.pageLoaded();
